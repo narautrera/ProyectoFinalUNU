5241acf078a507fc26fce91d14131af7af81db0a
narautrera <89810949+narautrera@users.noreply.github.com>
2021-11-22 21:17:59 -0300

    Add files via upload

---
+++ js/box.js
@@ -0,0 +1,14 @@
+function ampliar(x) {
+  x.style.width = "100%";
+  x.style.height = "100%";
+}
+function normal(x) {
+  x.style.width = "36%";
+  x.style.height = "36%";
+}
+function otrocartel() {
+  document.getElementById ("precio1").innerHTML = "$2000";
+}
+function cartel() {
+  document.getElementById ("precio2").innerHTML = "$2000";
+}
