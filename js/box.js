eb2e9439423cffed1db9a5f63b4d55348d9790e1
narautrera <89810949+narautrera@users.noreply.github.com>
2021-11-22 21:24:09 -0300

    Update box.js
    cambie precio

--- js/box.js
+++ js/box.js
@@ -7,8 +7,8 @@ function normal(x) {
   x.style.height = "36%";
 }
 function otrocartel() {
-  document.getElementById ("precio1").innerHTML = "$2000";
+  document.getElementById ("precio1").innerHTML = "$1900";
 }
 function cartel() {
-  document.getElementById ("precio2").innerHTML = "$2000";
+  document.getElementById ("precio2").innerHTML = "$1900";
 }
